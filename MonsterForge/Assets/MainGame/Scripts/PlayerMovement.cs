8cdb5ac9552f95352bbc3f5aae0f3b740f35274e
Wilson Tran <wilsontran2017@gmail.com>
2026-05-23 13:36:47 -0400

    added jump and new input manager system

--- MonsterForge/Assets/MainGame/Scripts/PlayerMovement.cs
+++ MonsterForge/Assets/MainGame/Scripts/PlayerMovement.cs
@@ -1,36 +1,56 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public InputAction playerMovement;
+    public PlayerInput playerMovement;
+    InputAction move;
+    InputAction jump;
+
     public float speed = 10;
-
+    public float jumpForce = 100;
     Rigidbody2D rb;
-    Vector2 moveDir = Vector2.zero;
-
+    float moveDir = 0;
+    private void Awake()
+    {
+        playerMovement = new PlayerInput();
+    }
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
     }
     private void OnEnable()
     {
-        if (playerMovement != null){ 
-            playerMovement.Enable(); 
-        }
+        move = playerMovement.Player.Move;
+        jump = playerMovement.Player.Jump;
+
+        move.Enable();
+        jump.Enable();
+
+        jump.performed += Jump; // register the function Jump to when the "playerMovement.Player.Jump" keybinding is pressed.
+
     }
     private void OnDisable()
     {
-        playerMovement.Disable();
+        move.Disable();
+        jump.Disable();
     }
     // Update is called once per frame
     void Update()
     {
-        moveDir = playerMovement.ReadValue<Vector2>();
+        moveDir = move.ReadValue<float>();
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = moveDir * speed;
+        rb.linearVelocityX = moveDir * speed;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("JUMP");
+        
+        rb.linearVelocityY = jumpForce;
     }
 }
